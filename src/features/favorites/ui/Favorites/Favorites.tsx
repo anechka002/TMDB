26ac4df6f7ee4fd74ef0f11b4e9d8e14b00dd789
Anna <blinova.anneta@gmail.com>
2025-12-15 13:29:56 +0100

    add logic in Favorites component

--- src/features/favorites/ui/Favorites/Favorites.tsx
+++ src/features/favorites/ui/Favorites/Favorites.tsx
@@ -1,7 +1,20 @@
+import {useAppSelector} from "@/common/hooks/useAppSelector.ts";
+import {
+  selectFavoritesMovies
+} from "@/features/favorites/model/favorites-slice.ts";
+import {MovieCard} from "@/entities/movie/ui/MovieCard.tsx";
+import s from "./Favorites.module.css";
+import {SectionTitle} from "@/common/components";
+
 export const Favorites = () => {
+  const favoritesMovies = useAppSelector(selectFavoritesMovies)
   return (
-    <div>
-      Favorites
-    </div>
+    <>
+      <SectionTitle title={'Favorites'}/>
+      <p className={s.title}>Favorite Movies</p>
+      <div className={s.favoritesCards}>
+        {favoritesMovies.map((movie) => (<MovieCard key={movie.id} movie={movie}/>))}
+      </div>
+    </>
   );
 };

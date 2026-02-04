f46049231fd248d80f7eb89e9f66ae23222fb796
Anna <blinova.anneta@gmail.com>
2026-02-04 10:48:42 +0100

    `Refactor PageNotFound component styles and layout`

--- src/shared/ui/PageNotFound/PageNotFound.tsx
+++ src/shared/ui/PageNotFound/PageNotFound.tsx
@@ -1,7 +1,25 @@
+import image from '../../assets/404.jpg'
+import s from './PageNotFound.module.css'
+import {NavLink} from "react-router";
+import {Path} from "@/app/routing";
+import {Button} from "@/shared";
+
 export const PageNotFound = () => {
   return (
-    <div>
-      PageNotFound
-    </div>
+    <section className={s.page}>
+      <div className={s.imageWrapper}>
+        <img
+          className={s.image}
+          src={image}
+          alt="image"
+        />
+      </div>
+      <p className={s.subtitle}>Page not found. We can’t find what you’re looking for</p>
+      <div>
+        <NavLink to={Path.Main}>
+          <Button>To main page</Button>
+        </NavLink>
+      </div>
+    </section>
   );
 };

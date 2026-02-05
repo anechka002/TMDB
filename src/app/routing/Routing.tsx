4f0e5c01cda4f5f72baba09ee1be7800ebb2cf5a
Anna <blinova.anneta@gmail.com>
2026-02-05 13:08:38 +0100

    `Added import of FavoritesPage in Routing.tsx and exported it from favorites/index.ts`

--- src/app/routing/Routing.tsx
+++ src/app/routing/Routing.tsx
@@ -1,12 +1,12 @@
 import {Navigate, Route, Routes} from "react-router";
 import {MainPage} from "@/pages/main/MainPage.tsx";
 import {Path} from "@/app/routing/paths.ts";
-import {FavoritesPage} from "@/pages/favorites/FavoritesPage.tsx";
 import {SearchPage} from "@/pages/search/SearchPage.tsx";
 import {MovieDetailsPage} from "@/pages/movie-details/MovieDetailsPage.tsx";
 import {FilteredMoviesPage} from "@/pages/filtered-movies";
 import {PageNotFound} from "@/shared";
 import {CategoryPage} from "@/pages/category";
+import {FavoritesPage} from "@/pages/favorites";
 
 export const Routing = () => (
   <Routes>
